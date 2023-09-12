dba2621f854ccfd9fa804a290c68ad420ff694c3
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2023-09-11 21:07:30 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,28 +8,5 @@ public class Program {
 		
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
-		
-		int abc1 = (char) 'a';
-		int abc2 = (char) 'b';
-		int abc3 = (char) 'c';
-		int abc4 = (char) 'd';
-		int abc5 = (char) 'e';
-		int abc6 = (char) 'f';
-		int abc7 = (char) 'g';
-		int abc8 = (char) 'h';
-		System.out.println();
-		
-		System.out.println(abc1);
-		System.out.println(abc2);
-		System.out.println(abc3);
-		System.out.println(abc4);
-		System.out.println(abc5);
-		System.out.println(abc6);
-		System.out.println(abc7);
-		System.out.println(abc8);
-		System.out.println();
-		
-		char cba = (int) 101;
-		System.out.println(cba);
 	}
 }
