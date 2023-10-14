a3424ab50cf621e40275e552f8c090b5d5f6c09f
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2023-10-13 21:28:46 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,10 +56,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println(ANSI_RED + "Check!" + ANSI_RESET);
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
+				System.out.println(ANSI_RED + "Check!" + ANSI_RESET);
+			}
+		} else {
+			System.out.println(ANSI_GREEN + "CHECKMATE!" + ANSI_RESET);
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
+		
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
